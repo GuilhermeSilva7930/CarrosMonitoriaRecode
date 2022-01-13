f639a5556bda971967e259140c4ee83106cd0340
Guilherme Ferreira Silva <guilhermeferreira7930@gmail.com>
2022-01-13 13:16:22 -0300

    Adicionar arquivos de projeto.

---
+++ CarrosMonitoria/Models/CadastrarCarro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarrosMonitoria.Models
+{
+    public class CadastrarCarro
+    {
+        [Key]
+        [Required]
+        public int Id_Carro { get; set; }
+        [Required]
+        public string Nome_Carro { get; set; }
+        [Required]
+        public string Preco { get; set; }
+        [Required]
+        public int Ano { get; set; }
+        public virtual List<Comprar> Comprar { get; set; }
+    }
+}
